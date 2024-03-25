f75e7b5ca091ea08c8d0ef676fd1ba6e60863669
IronLegends7519 <drafin@edenschool.fr>
2024-03-25 15:11:14 +0100

    finish fonctionnalité

--- js/app.js
+++ js/app.js
@@ -20,32 +20,83 @@ const tableauImage = [{
     alt:"John Tarkpor- Junior Front-end developer",
     job:'Junior Front-end developer',
     text:'Si vous souhaitez poser les meilleures bases possibles, je vous recommande de suivre ce cours. Il m’a aidé à prendre confiance en moi et à devenir un meilleur développeur.'
+},
+{
+    id:3, 
+    image:'./images/image-bob.jpg', 
+    name:'Bob Razvoski',
+    alt:"Bob Razvoski- Junior Back-end developer",
+    job:'Junior Back-end developer',
+    text: 'Si vous souhaitez poser les meilleures bases possibles, je vous recommande de suivre ce cours. Il m’a aidé à prendre confiance en moi et à devenir un meilleur développeur.'
+},
+{
+    id:4, 
+    image:'./images/image-toto.jpg', 
+    name:'Toto',
+    alt:"Toto- Developer",
+    job:'Developer',
+    text: `La programmation m'intéressait depuis un certain temps mais je n’avais jamais sauté le pas. Ce cours a été une révélation pour moi. Il m’a permis d’obtenir le job de mes rêves.`
 }
 ]
 
 let index = 0
 
+function modifierHtml (){
+
+    const tableauIndex = tableauImage[index%4];
+
+    images.setAttribute('src',tableauIndex.image)
+    images.setAttribute('alt',tableauIndex.alt)
+    prénom.textContent = tableauIndex.name
+    métier.textContent = tableauIndex.job
+    texte.textContent = tableauIndex.text
+}
 
     next.addEventListener('click',()=>{
-        index++
-        modifierHtml ()
+        nextFunction()
     })
     
     previous.addEventListener('click',()=>{
+        previousFunction()
+    })
+
+
+    document.addEventListener('keydown',(event)=>{
+        switch(event.key){
+            case 'ArrowLeft':
+                previousFunction()
+                break;
+            case 'ArrowRight':
+                nextFunction()
+        }
+    })
+
+
+    let interval = setInterval(()=>{
+        index++
+        modifierHtml()
+    },10000)
+
+
+    function previousFunction(){
         index--
         if(index === -1){
             index=tableauImage.length-1
         }
         modifierHtml ()
-    })
+        clearInterval(interval)
+        interval = setInterval(()=>{
+            index++
+            modifierHtml()
+        },10000)
+    }
 
-    function modifierHtml (){
-        
-        const tableauIndex = tableauImage[index%2];
-
-        images.setAttribute('src',tableauIndex.image)
-        images.setAttribute('alt',tableauIndex.alt)
-        prénom.textContent = tableauIndex.name
-        métier.textContent = tableauIndex.job
-        texte.textContent = tableauIndex.text
+    function nextFunction (){
+        index++
+        modifierHtml ()
+        clearInterval(interval)
+        interval = setInterval(()=>{
+            index++
+            modifierHtml()
+        },10000)
     }
